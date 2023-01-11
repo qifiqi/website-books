83cd11883f5dedd1e55b992a0c40b2d0904cbd90
fuqing <2737454073@qq.com>
2023-01-11 22:28:53 +0800

    表白

--- creative_page/views.py
+++ creative_page/views.py
@@ -3,10 +3,11 @@ from django.views.decorators.cache import cache_page
 
 
 # Create your views here.
-@cache_page(60*60)
+@cache_page(60 * 60)
 def jtcsy(requests):
     return render(requests, "creative_page/今天吃啥呀？.html")
 
-@cache_page(60*60)
+
+@cache_page(60 * 60)
 def xiaofu_xiaochen(requests):
     return render(requests, "creative_page/表白.html")
